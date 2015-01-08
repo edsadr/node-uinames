8c5269a983af9321ef935a2e4beed31610dfdb34
Julian Duque <julianduquej@gmail.com>
2015-01-08 18:11:13 -0500

    Refactor uiname
    * Remove jsonQuery in favor or ES5 array methods
    * Use require() to get JSON file readFile & parse isn't needed
    * Minor code formatting

--- uiname.js
+++ uiname.js
@@ -1,20 +1,19 @@
 'use strict';
 
-var fs = require('fs');
-var defaults = require('defaults');
-var jsonQuery = require('json-query');
+var fs = require('fs'),
+    defaults = require('defaults');
 
 module.exports = function(args, callback) {
 
-  //Function extracting a random element from a given array
-  var randomElement = function(arrElrment) {
-    return arrElrment[Math.floor(Math.random() * arrElrment.length)];
+  // Function extracting a random element from a given array
+  var randomElement = function(array) {
+    return array[Math.floor(Math.random() * array.length)];
   };
 
   //Defaults
-  var options = {    
+  var options = {
     amount: 1,
-    country: 'Colombia',//que viva Colombia
+    country: 'Colombia', //que viva Colombia
     gender: false
   };
 
@@ -23,44 +22,43 @@ module.exports = function(args, callback) {
 
   var results = [];
 
-  //Reading the file previously dowloaded at the postinstall script
-  fs.readFile('names.json', function(err, data) {
-    if (err) {
-      return callback(err);
-    }
+  // Require the names file
+  var source;
+  try {
+    source = require('./names');
+  } catch (e) {
+    return callback(new Error("Source names not found, please run `getsource.js` script"));
+  }
 
-    //Parsing the data
-    var source = JSON.parse(data);    
-    var countries = source.map(function(index){
-      return index.country;
-    });
-
-
-    //Querying
-    var target = jsonQuery('[country=' + query.country + ']', {
-      data: source
-    }).value;
-
-    //Checking if the country is there
-    if(!target){
-      var cerr= new Error('Your country is not available, valid options are: '+countries.toString());
-      return callback(cerr);
-    }
-
-
-    //Generating the names
-    for (var i = 0; i < query.amount; i++) {
-      var gender = (query.gender)?query.gender:randomElement(['male', 'female']);
-      
-      var person = {
-        firstName: randomElement(target[gender]),
-        lastName: randomElement(target.surnames),
-        gender: gender
-      };
-
-      results.push(person);
-    }
-
-    return callback(null, results);
+  var countries = source.map(function(index){
+    return index.country;
   });
-}
+
+  var target = source.filter(function (data) {
+    return data.country.toLowerCase() === query.country.toLowerCase();
+  });
+
+  // Checking if the country is there
+  if (!target || target.length < 1) {
+    return callback(
+      new Error('Your country is not available, valid options are: ' + countries.toString())
+    );
+  }
+
+  var country = target[0];
+
+  // Generate the names
+  for (var i = 0; i < query.amount; i++) {
+    var gender = (query.gender) ? query.gender : randomElement(['male', 'female']);
+
+    var person = {
+      firstName: randomElement(country[gender]),
+      lastName: randomElement(country.surnames),
+      gender: gender
+    };
+
+    results.push(person);
+  }
+
+  callback(null, results);
+};
