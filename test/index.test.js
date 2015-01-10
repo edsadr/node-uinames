72422fb3c7ed7419f5fe6944ac0c7087d0115c42
Adrian Estrada <edsadr@gmail.com>
2015-01-10 07:17:38 -0500

    Completing testing

--- test/index.test.js
+++ test/index.test.js
@@ -5,23 +5,54 @@ var uinames = require('../uinames.js')
 
 
 describe('uinames', function () {
-	uinames({country: 'Nocountry', amount: 1}, function(err,person){
-		it('Should be an error',function(){
-			expect(err).not.to.be.null();
-		});
+  uinames({
+    country: 'Nocountry'
+  }, function (err, person) {
+    it('Should be an error', function () {
+      expect(err).not.to.be.null();
+    });
 
-		it('Should not have a person',function(){
-			expect(person).to.be.undefined();
-		});
-	});
+    it('Should not have a person', function () {
+      expect(person).to.be.undefined();
+    });
+  });
 
-	uinames({country: 'Colombia', amount: 1}, function(err,person){
-		it('Should not to be an error',function(){
-			expect(err).to.be.null();
-		});
+  uinames({
+    amount: 1
+  }, function (err, person) {
+    it('Should not to be an error', function () {
+      expect(err).to.be.null();
+    });
 
-		it('Should have a person',function(){
-			expect(person).not.to.be.null();
-		});
-	});
+    it('Should have a person', function () {
+      expect(person).not.to.be.undefined();
+    });
+
+    it('Should have a gender', function () {
+      expect(person[0]).to.include.keys('gender');
+    });
+
+    it('Should be male or female', function () {
+      expect(person[0].gender).to.satisfy(function (gender) {
+        return gender === 'male' || gender === 'female';
+      });
+    });
+
+    it('Should be from Colombia', function () {
+      expect(person[0].country).to.be.equal('Colombia');
+    });
+  });
+
+  uinames({
+    country: 'United States',
+    amount: 10
+  }, function (err, person) {
+    it('Should have 10 people', function () {
+      expect(person.length).to.be.equal(10);
+    });
+
+    it('Should be from USA', function () {
+      expect(person[0].country).to.be.equal('United States');
+    });
+  });
 })
