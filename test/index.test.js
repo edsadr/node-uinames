650624c53dfa73679520595f8bc729fbab7a9683
Adrian Estrada <edsadr@gmail.com>
2015-01-09 23:25:17 -0500

    Basic testing done

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,27 @@
+'use strict';
+
+var expect = require('chai').expect;
+var uinames = require('../uinames.js')
+
+
 describe('uinames', function () {
+	uinames({country: 'Nocountry', amount: 1}, function(err,person){
+		it('Should be an error',function(){
+			expect(err).not.to.be.null();
+		});
 
-  it('should work as expected', function (done) {
-    done()
-  })
+		it('Should not have a person',function(){
+			expect(person).to.be.undefined();
+		});
+	});
 
+	uinames({country: 'Colombia', amount: 1}, function(err,person){
+		it('Should not to be an error',function(){
+			expect(err).to.be.null();
+		});
+
+		it('Should have a person',function(){
+			expect(person).not.to.be.null();
+		});
+	});
 })
