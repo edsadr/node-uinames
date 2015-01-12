60535701d0a84f4223e4649046ee460d28a5dd14
Adrian Estrada <edsadr@gmail.com>
2015-01-11 22:26:02 -0500

    Adding the option to update the file

--- getsource.js
+++ getsource.js
@@ -4,9 +4,25 @@ var request = require('request'),
 
 var namesUrl = 'https://raw.githubusercontent.com/thm/uinames/master/names.json';
 
-fs.stat('names.json', function (err, stat) {
-  if (err || !stat) {
-    request.get(namesUrl)
-      .pipe(fs.createWriteStream('names.json'));
-  }
-});
+if (process.argv[2] === 'create') {
+  createFile();
+} else if (process.argv[2] === 'update') {
+  deleteFile();
+  createFile();
+}
+
+function deleteFile() {
+  fs.unlink('names.json', function (err) {
+    if (err) throw err;
+    	
+  });
+}
+
+function createFile() {
+  fs.stat('names.json', function (err, stat) {
+    if (err || !stat) {
+      request.get(namesUrl)
+        .pipe(fs.createWriteStream('names.json'));
+    }
+  });
+}
